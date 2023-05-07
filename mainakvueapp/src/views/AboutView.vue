3fc99c54d74546f480eae977f656702f32163bee
dutmain <85560325+dutmain@users.noreply.github.com>
2023-05-07 17:23:34 +0530

    test

---
+++ mainakvueapp/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Do you like this app?</h1>
+    <button v-on:click="Yes">Yes</button>
+    <button v-on:click="No">No</button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    Yes() {
+      alert("Thank you!");
+    },
+    No() {
+      alert("I'm sorry!");
+    },
+  },
+};
+</script>
